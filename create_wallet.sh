2cce37c5c75fc5ed8bd4ee1bd5c2fee34082f473
nateg98 <nateg98@vt.edu>
2020-02-22 14:48:33 -0500

    Finished the script

--- create_wallet.sh
+++ create_wallet.sh
@@ -5,4 +5,13 @@ if [ -z "$1" ]
       echo "No argument supplied"
 fi
 
-cleos wallet create -f "$1_wallet_key"
+cleos wallet create -f "$1_wallet_key.txt"
+mv default.wallet $1.wallet
+
+cleos wallet unlock < $1_wallet_key.txt
+
+cleos wallet create_key -n $1 > $1_pub_key.txt
+
+cleows wallet import -n $1 --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
+
+cleos create account eosio $1 $1_pub_key.txt
