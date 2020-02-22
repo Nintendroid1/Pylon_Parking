ddf8b7204ed0d5a081bf8add552f2452b686af11
Nintendroid1 <nlpsalazar@gmail.com>
2020-02-22 14:59:02 -0500

    cleos misspell

--- create_wallet.sh
+++ create_wallet.sh
@@ -13,6 +13,6 @@ cleos wallet unlock < $1_wallet_key.txt
 
 cleos wallet create_key -n $1 > $1_pub_key.txt
 
-cleows wallet import -n $1 --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
+cleos wallet import -n $1 --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
 
 cleos create account eosio $1 $1_pub_key.txt
