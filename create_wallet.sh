dfc9dc9cda7e6228ccca5ce0cf101aa4d6b78c89
nateg98 <nateg98@vt.edu>
2020-02-27 10:47:40 -0500

    Fixed create wallet

--- create_wallet.sh
+++ create_wallet.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -z "$1" ]
-   then
-      echo "No argument supplied"
-      exit 1
+    then
+        echo "No argument supplied"
+        exit 1
 fi
 
 cleos wallet create -n $1 -f "$1_wallet_key.txt"
@@ -14,4 +14,8 @@ cleos wallet create_key -n $1 > $1_pub_key.txt
 
 cleos wallet import -n $1 --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
 
-cleos create account eosio $1 $1_pub_key.txt
+var=`cat $1_pub_key.txt`
+cleos create account eosio $1 ${var:47:53}
+
+mv $1_wallet_key.txt eosio-wallet
+mv $1_pub_key.txt eosio-wallet
