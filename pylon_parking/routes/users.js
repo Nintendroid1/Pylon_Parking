05bd48fe46491bba7955c85e3f9b9df72eb67c41
nateg98 <nateg98@vt.edu>
2020-03-03 15:12:23 -0500

    skeleton for login response

--- pylon_parking/routes/users.js
+++ pylon_parking/routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/', function(req, res, next) {
+  //Do database check and jwt creation here
+  
+  let jwt = 69;
+  res.status(200).send(jwt);
+});
+
 module.exports = router;
