6f72303ac468a623f0e1907009c578879d132c77
nateg98 <nateg98@vt.edu>
2020-03-04 10:00:24 -0500

    Json instead of weird

--- api/users.js
+++ api/users.js
@@ -14,7 +14,11 @@ router.post('/', (req,res) => {
     const isValidLogin = true;
 
     if(isValidLogin) {
-        res.status(200).send(jwt.createJWT());
+        res.status(200).json({
+            token : jwt.createJWT(),
+            user : 'this is a json with username and admin status',
+            message: `Logged in as `
+        });
     }
     else {
         res.status(401).json({message:'Invalid Login'});
