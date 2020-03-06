b5039fedac9cb395b7877d08c9b4636275dac040
Evan Ott <evan1533@vt.edu>
2020-03-06 12:12:07 -0500

    Moved node.js server backend files to 'backend' subdirectory. This should help facilitate easier integration

---
+++ backend/api/jwt.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+//TODO get secret and set it here
+const secret = '';
+
+function createJWT() {
+    //jwt.sign()
+    return null;
+}
+
+function verifyJWT() {
+    //jwt.verify()
+    return null;
+}
+
+const funcs = {
+    createJWT,
+    verifyJWT
+}
+module.exports = funcs;
---
+++ backend/api/users.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('./jwt');
+
+const { Client } = require('pg');
+const client = new Client();
+
+router.get('/', (req, res) => {
+    res.send("This should be login api");
+});
+
+router.post('/login', (req,res) => {
+    //Check database
+    const isValidLogin = true;
+
+    if(isValidLogin) {
+        res.status(200).json({
+            token : jwt.createJWT(),
+            user : 'this is a json with username and admin status',
+            message: `Logged in as `
+        });
+    }
+    else {
+        res.status(401).json({message:'Invalid Login'});
+    }
+});
+
+router.post('/register', (req,res) => {
+    //Check database for duplicate username/email
+    //Add into database new user stuff
+    //do login
+});
+module.exports = router;
