24a8f1433e6023e6787c9c6de55eee2079816dc6
Evan Ott <evan1533@vt.edu>
2020-03-25 19:17:56 -0400

    Messing around with git tags

--- mark_minor.sh
+++ mark_minor.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-git describe --tags $(git rev-list --tags --max-count=1)
+BRANCH_NAME="frontend"
+latestTag=$(git describe --tags --abbrev=0 $BRANCH_NAME)
+
+latestVersion=${latestTag//-*/ }
+latestVersion=${latestVersion#v}
+versionBits=(${latestVersion//./ })
+latestMajor=${versionBits[0]}
+latestMinor=${versionBits[1]}
+echo "$latestVersion"
+echo "$latestMajor"
+echo "$latestMinor"
+
+next_minor="v$latestMajor.$(( latestMinor + 1 ))-$BRANCH_NAME"
+echo "Next Minor: $next_minor"
