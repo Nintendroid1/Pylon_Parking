ffdaeb0162e07fcebbe74bfe7f7e635daa6dc54e
Evan Ott <evan1533@vt.edu>
2020-03-25 19:38:49 -0400

    Finished mark minor git tag script

--- mark_minor.sh
+++ mark_minor.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-BRANCH_NAME="frontend"
-latestTag=$(git describe --tags --abbrev=0 $BRANCH_NAME)
+function mark_minor_tag() {
+    BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+    latestTag=$(git describe --tags --abbrev=0 $BRANCH_NAME)
 
-latestVersion=${latestTag//-*/ }
-latestVersion=${latestVersion#v}
-versionBits=(${latestVersion//./ })
-latestMajor=${versionBits[0]}
-latestMinor=${versionBits[1]}
-echo "$latestVersion"
-echo "$latestMajor"
-echo "$latestMinor"
+    latestVersion=${latestTag//-*/ }
+    latestVersion=${latestVersion#v}
+    versionBits=(${latestVersion//./ })
+    latestMajor=${versionBits[0]}
+    latestMinor=${versionBits[1]}
+    # echo "$latestVersion"
+    # echo "$latestMajor"
+    # echo "$latestMinor"
 
-next_minor="v$latestMajor.$(( latestMinor + 1 ))-$BRANCH_NAME"
-echo "Next Minor: $next_minor"
+    next_minor="$(( latestMinor + 1 ))"
+    NEW_TAG="v$latestMajor.$next_minor-$BRANCH_NAME"
+    echo "Next Minor: $next_minor"
+    echo "New Tag: $NEW_TAG"
+    git tag $NEW_TAG
+}
+
+#Get current hash and see if it already has a tag
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT`
+
+##Only tag if no tag already (would be better if the git describe command above could have a silent option)
+if [ -z "$NEEDS_TAG" ]; then
+    echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
+    mark_minor_tag
+else
+    echo "Already a tag on this commit"
+fi
+
 
 
 
