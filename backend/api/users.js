0be55ea8f1d8d199946655e9ef2a0b99efd1c9a6
nateg98 <nateg98@vt.edu>
2020-03-25 10:34:40 -0400

    Working on registering

--- backend/api/users.js
+++ backend/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('./jwt');
+const db = require('../db');
 
 router.get('/', (req, res) => {
     res.send("This should be login api");
@@ -24,7 +25,28 @@ router.post('/login', (req,res) => {
 
 router.post('/register', (req,res) => {
     //Check database for duplicate username/email
+    db.query('SELECT PID FROM users WHERE PID = $1 OR email = $2', [req.user.pid, req.user.email], (err, dbres) => {
+        if (err) {
+            console.log(err.stack);
+        } else {
+            //check for duplicate email and username, if there is a duplicate
+            //send error if there is and return
+            for(i in dbres.rows) {
+                console.log(dbres.rows[i]);
+            }
+        }
+    });
+
     //Add into database new user stuff
+    const text = 'INSERT INTO users(PID, pass, email, first_name, last_name, admin_status) VALUES($1, $2, $3, $4, $5, false) RETURNING *';
+    const values = [req.user.PID, req.user.pass, req.user.email, req.user.first_name, req.user.last_name];
+    db.query(text, values, (err,dbres) => {
+        if (err) {
+            console.log(err.stack);
+        } else {
+            console.log(dbres.rows[0]);
+        }
+    });
     //do login
 });
 module.exports = router;
--- backend/db/index.js
+++ backend/db/index.js
@@ -1,5 +1,9 @@
 const { Pool } = require('pg')
-const pool = new Pool()
+const pool = new Pool({
+  user: 'postgres',
+  database: 'PylonParking',
+  password: 'superman',
+});
 module.exports = {
   query: (text, params, callback) => {
     return pool.query(text, params, callback)
