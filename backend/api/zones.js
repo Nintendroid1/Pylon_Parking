f7f380127121e933009c433831e7b622ffbba7a1
nateg98 <nateg98@vt.edu>
2020-03-26 15:23:08 -0400

    Added baseline for purchasing spots

--- backend/api/zones.js
+++ backend/api/zones.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../db');
 
 router.get('/all', (req, res) => {
-    db.query('SELECT zone_id, name FROM zones', (err,dbres) => {
+    db.query('SELECT zone_id, zone_name FROM zones', (err,dbres) => {
         if (err) {
             console.log(err.stack);
             res.status(500).json({message: "Internal server error"});
@@ -15,11 +15,27 @@ router.get('/all', (req, res) => {
 });
 
 router.get('/:zone_id/spot/:spot_id', (req, res) => {
-    res.send("This should list details of a particular spot");
+    db.query('SELECT * FROM parking_times WHERE spot_ID = $1 AND zone_ID = $2',[req.params.zone_id, req.params.spot_id], (err,dbres) => {
+        if (err) {
+            console.log(err.stack);
+            res.status(500).json({message: "Internal server error"});
+        }
+        else {
+            res.status(200).json(dbres.rows);
+        }
+    })
 });
 
 router.get('/:zone_id', (req, res) => {
-    res.send("This should list all the spots in a zone");
+    db.query('SELECT spot_id FROM parking_spots WHERE zone_ID = $1',[req.params.zone_id], (err,dbres) => {
+        if (err) {
+            console.log(err.stack);
+            res.status(500).json({message: "Internal server error"});
+        }
+        else {
+            res.status(200).json(dbres.rows);
+        }
+    })
 });
 
 module.exports = router;
--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,13 @@ const app = express();
 const indexRouter =  require('./api/index');
 const usersRouter = require('./api/users');
 const zonesRouter = require('./api/zones');
+const purchaseRouter = require('./api/purchase');
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/zones', zonesRouter);
+app.use('/purchase', purchaseRouter);
 app.use(express.json());
 
 app.use(function (req, res, next) {
