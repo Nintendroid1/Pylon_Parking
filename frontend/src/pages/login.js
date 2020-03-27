b9dc9f821402544a7a49e7325b3e82faff1600b1
Evan Ott <evan1533@vt.edu>
2020-03-27 01:53:56 -0400

    Fixed api request in login.js

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -37,7 +37,10 @@ const LoginTab = ({
   // handle user login
   const userLogin = async values => {
     setLoading(true);
-    let res = await makeAPICall('POST', `${apiprefix}/login`, values);
+    console.log(`Making POST request to: ${apiprefix}/users/login`)
+    console.log('Using values:')
+    console.log(values)
+    let res = await makeAPICall('POST', `${apiprefix}/users/login`, values);
     let body = await res.json();
 
     setLoading(false);
