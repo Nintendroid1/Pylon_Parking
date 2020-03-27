3d4d5780ef27bc53aed7beb78b46f33391623817
BuildTools <unconfigured@null.spigotmc.org>
2020-03-27 02:02:08 -0400

    Modified the zone sql queries in api/zones.js to return the info formatted according to the frontend files

--- backend/api/zones.js
+++ backend/api/zones.js
@@ -9,31 +9,35 @@ router.get('/all', (req, res) => {
             res.status(500).json({message: "Internal server error"});
         }
         else {
-            res.status(200).json(dbres.rows);
+			console.log(dbres.rows);
+            res.status(200).json({zones: dbres.rows});
         }
     })
 });
 
 router.get('/:zone_id/spot/:spot_id', (req, res) => {
-    db.query('SELECT * FROM parking_times WHERE spot_ID = $1 AND zone_ID = $2',[req.params.zone_id, req.params.spot_id], (err,dbres) => {
+    console.log(req.params)
+    db.query('SELECT * FROM parking_times WHERE spot_ID = $1 AND zone_ID = $2',[ req.params.spot_id, req.params.zone_id ], (err,dbres) => {
         if (err) {
             console.log(err.stack);
             res.status(500).json({message: "Internal server error"});
         }
         else {
-            res.status(200).json(dbres.rows);
+            console.log(dbres)
+            res.status(200).json({parkingInfo: dbres.rows});
         }
     })
 });
 
 router.get('/:zone_id', (req, res) => {
-    db.query('SELECT spot_id FROM parking_spots WHERE zone_ID = $1',[req.params.zone_id], (err,dbres) => {
+    // db.query('SELECT spot_id FROM parking_spots WHERE zone_ID = $1',[req.params.zone_id], (err,dbres) => {
+    db.query('SELECT spot_id, zone_id, MIN(time_code) "next_avail" FROM parking_times where zone_id = $1 GROUP BY zone_id, spot_id ORDER BY spot_id ASC',[req.params.zone_id], (err,dbres) => {
         if (err) {
             console.log(err.stack);
             res.status(500).json({message: "Internal server error"});
         }
         else {
-            res.status(200).json(dbres.rows);
+            res.status(200).json({parkingInfo: dbres.rows});
         }
     })
 });
