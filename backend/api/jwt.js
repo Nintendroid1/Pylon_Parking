20b64417148676b7a3e805f79fe7aec319baf07e
Evan Ott <evan1533@vt.edu>
2020-03-28 00:39:34 -0400

    Added admin status to jwt

--- backend/api/jwt.js
+++ backend/api/jwt.js
@@ -1,23 +1,30 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 //TODO get secret and set it here
-const secret = 'verysecretpasswordthatsdefinatelynotinthesource';
+const secret = "verysecretpasswordthatsdefinatelynotinthesource";
 
-function createJWT(userName) {
-    return jwt.sign({exp: Math.floor(Date.now() / 1000) + (60 * 60), user: userName,}, secret);
+function createJWT(userName, admin_status) {
+  return jwt.sign(
+    {
+      exp: Math.floor(Date.now() / 1000) + 60 * 60,
+      pid: userName,
+      admin: admin_status
+    },
+    secret
+  );
 }
 
-function verifyJWT(token,userName) {
-    try {
-        let decoded = jwt.verify(token, secret);
-        return decoded.user === userName;
-    } catch(err) {
-        return false;
-    }
+function verifyJWT(token, userName) {
+  try {
+    let decoded = jwt.verify(token, secret);
+    return decoded.pid === userName;
+  } catch (err) {
+    return false;
+  }
 }
 
 const funcs = {
-    createJWT,
-    verifyJWT
-}
+  createJWT,
+  verifyJWT
+};
 module.exports = funcs;
