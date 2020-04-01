62aa92defa2108f7603403c0d1a0521c40fac64a
Nintendroid1 <nlpsalazar@gmail.com>
2020-04-01 12:23:12 -0400

    Transfer tokens within contract

--- src/parkingspot.cpp
+++ src/parkingspot.cpp
@@ -1,16 +1,76 @@
 #include <eosio/eosio.hpp>
 #include <eosio/print.hpp>
 #include <eosio/system.hpp>
+#include <eosio/asset.hpp>
 #include <vector>
 
 using namespace eosio;
 
 class [[eosio::contract("parkingspot")]] parkingspot : public eosio::contract {
+private:
+  const symbol hokie_coin;
+
+  struct [[eosio::table]] balance
+  {
+    eosio::asset funds;
+    uint64_t primary_key() const { return funds.symbol.raw(); }
+  };
+
+  using balance_table = eosio::multi_index<"balance"_n, balance>;
+
+  uint32_t now() {
+    return current_time_point().sec_since_epoch();
+  }
+
+  //Time to expire is now() + 1 hour
+  uint32_t start = now();
+  const uint32_t trans_expire = start + 3600;
+
+  //Parking spot to store the info
+  struct [[eosio::table]] pspot {
+    uint64_t spot_id; //key
+    uint64_t zone_id;
+    std::vector<uint32_t>  timeclock; //Time information in 15 min intervals
+    bool available;
+    name owner;
+
+    //Sets primary key to be the spot id
+    uint64_t primary_key() const {return spot_id;}
+  };
+
+  void send_summary(name user, std::string message) {
+    /*
+    * Permision level = authorized is the active authority of the contract
+    * Code = account where contract is deployed
+    * Action we want to perform
+    * Data to pass to the action
+    */
+    action (
+      permission_level{get_self(),"active"_n},
+      get_self(),
+      "notify"_n,
+      std::make_tuple(user, name{user}.to_string() + message)
+    ).send();
+  };
+
+  /*_n is the name of the table
+  * takes in a pspot value
+  */
+  typedef eosio::multi_index<"pspots"_n, pspot> park_index;
 
 public:
  //"code" param is the account the contract is deployed to
-  parkingspot(name receiver, name code, datastream<const char*> ds):contract(receiver, code, ds){}
+ //Will only accept hokie coin of type VTP with 4 decimal places
+  parkingspot(name receiver, name code, datastream<const char*> ds):contract(receiver, code, ds),hokie_coin("VTP", 4){}
   
+  [[eosio::action]]
+  void notify(name user, std::string msg) {
+    //Only authorization is the contract itself
+    require_auth(get_self());
+    //Ensures the accounts receive the notificaiton of the action being executed (carbon copy)
+    require_recipient(user);
+  }
+
   //Inserts new parking spot
   [[eosio::action]] //Needed for ABI generation
   void insert(name user, uint64_t spot_id, uint64_t zone_id, uint32_t time_code, name owner) {
@@ -71,10 +131,26 @@ public:
   }
 
   //Updates parking spot available
-  [[eosio::action]]
-  void modavail(name user, uint64_t spot_id, uint64_t zone_id, uint32_t time_code, name owner) {
+  //[[eosio::action]]
+  [[eosio::on_notify("eosio.token::transfer")]]
+  void modavail(name user, eosio::asset quantity, uint64_t spot_id, uint64_t zone_id, uint32_t time_code, name owner) {
+    //Ensure not transferring to self
+    // if (owner == get_self() || user != get_self()) {
+    //   return;
+    // }
       //Ensures the account executing transaction has proper permissions
       require_auth(user);
+
+      //The transaction didn't take an hour
+      check(now() < trans_expire, "Transfer time expired");
+      
+      //The incoming transfer has a valid amount of tokens
+      check(quantity.amount > 0, "Invalid Amount");
+
+      //The incoming transfer uses the token we specify in the constructor
+      //print(quantity.symbol, " ", hodl_symbol);
+      check(quantity.symbol == hokie_coin, "Incorrect Currency Type");
+
 
       /* Creates Table to index from
       *  first param specifies the owner of this table
@@ -93,17 +169,37 @@ public:
             print("DOES NOT EXIST! Parking Spot: ", spot_id, " in Zone: ", zone_id);
         }
         else {
+            //Get money from user
+            balance_table balance(get_self(), user.value);
+            auto bal_it = balance.find(hokie_coin.raw());
 
-            // bool status_bool = false;
-            // std::string status_str = "occupied";
-            // //The parking spot is in the table
-            // parkdeck.modify(iterator, user, [&](auto& row) {
-            //     row.available = !(row.available);
-            //     status_bool = row.available;
-            // });
-            // if(status_bool) {
-            //   status_str = "not occupied";
-            // }
+            if (bal_it != balance.end())
+              //Subtract from exisiting money
+              balance.modify(bal_it, get_self(), [&](auto &row) {
+                if(row.funds < quantity) {
+                  print("Insufficient Funds From User");
+                  return;
+                }
+                row.funds -= quantity;
+              });
+            else
+                print("No Account Found for User");
+              }
+
+            //Transfer money to old owner
+            balance_table balance(get_self(), owner.value);
+            auto bal_it = balance.find(hokie_coin.raw());
+
+            if (bal_it != balance.end())
+              //Add to exisiting money
+              balance.modify(bal_it, get_self(), [&](auto &row) {
+                row.funds += quantity;
+              });
+            else
+              //Give new money
+              balance.emplace(get_self(), [&](auto &row) {
+                row.funds = quantity;
+              });
 
             parkdeck.modify(iterator, user, [&](auto& row) {
                 bool found = false;
@@ -115,8 +211,8 @@ public:
                 if(!found) {
                   row.timeclock.push_back(time_code);
                 }
-
-                send_summary(row.owner, " transferring ownership");
+                require_auth(row.owner);
+                //end_summary(row.owner, " transferring ownership");
                 row.available = false;
                 row.owner = user;
             });
@@ -124,55 +220,4 @@ public:
             print("Parking Spot: ", spot_id, " in Zone: ", zone_id, " is owned by ", user, " for: ", time_code, ". Transaction on ", current_time_point().sec_since_epoch());
             send_summary(user, " successfully changed parking spot availability");
         }
-  }
-
-  [[eosio::action]]
-  void notify(name user, std::string msg) {
-    //Only authorization is the contract itself
-    require_auth(get_self());
-    //Ensures the accounts receive the notificaiton of the action being executed (carbon copy)
-    require_recipient(user);
-  }
-
-private:
-  // 
-  // struct [[eosio::table]] ttime {
-  //   uint32_t time_code;
-
-  // //Sets primary key to be the time
-  // uint64_t primary_key() const {return spot_id;}
-  // };
-  // typedef eosio::multi_index<"ttimes"_n, ttime> time_index;
-
-  //Parking spot to store the info
-  struct [[eosio::table]] pspot {
-    uint64_t spot_id; //key
-    uint64_t zone_id;
-    std::vector<uint32_t>  timeclock; //Time information in 15 min intervals
-    bool available;
-    name owner;
-
-    //Sets primary key to be the spot id
-    uint64_t primary_key() const {return spot_id;}
-  };
-
-  void send_summary(name user, std::string message) {
-    /*
-    * Permision level = authorized is the active authority of the contract
-    * Code = account where contract is deployed
-    * Action we want to perform
-    * Data to pass to the action
-    */
-    action (
-      permission_level{get_self(),"active"_n},
-      get_self(),
-      "notify"_n,
-      std::make_tuple(user, name{user}.to_string() + message)
-    ).send();
-  };
-
-  /*_n is the name of the table
-  * takes in a pspot value
-  */
-  typedef eosio::multi_index<"pspots"_n, pspot> park_index;
 };
