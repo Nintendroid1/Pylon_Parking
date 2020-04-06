c2434e3e7d2806eef597f3ce5abde6d69f52ebc3
nateg98 <nateg98@vt.edu>
2020-04-06 13:28:50 -0400

    Required auth, forgot to do that earlier

--- backend/api/sell.js
+++ backend/api/sell.js
@@ -7,37 +7,41 @@ const db = require('../db');
 router.use(express.json());
 
 router.post('/',(req,res) => {
-    if(req.body.pid && req.body.token && req.body.spot.spot_id && req.body.spot.zone_id && req.body.spot.start_time && req.body.spot.end_time && req.body.spot.price) {
+    if(jwt.verifyJWT(req.body.token, req.body.userName)) {
+        if(req.body.pid && req.body.token && req.body.spot.spot_id && req.body.spot.zone_id && req.body.spot.start_time && req.body.spot.end_time && req.body.spot.price) {
 
-        let isValidReq = true;
-        db.query("SELECT * FROM parking_times WHERE spot_id = $1 AND zone_id = $2 AND time_code BETWEEN $3 AND $4",
-            [req.body.spot.spot_id, req.body.spot.zone_id, req.body.spot.start_time, req.body.spot.end_time-1])
-        .then(dbres => {
-            for(i in dbres.rows) {
-                if(dbres.rows[i].user_pid != req.body.pid) {
-                    isValidReq = false;
+            let isValidReq = true;
+            db.query("SELECT * FROM parking_times WHERE spot_id = $1 AND zone_id = $2 AND time_code BETWEEN $3 AND $4",
+                [req.body.spot.spot_id, req.body.spot.zone_id, req.body.spot.start_time, req.body.spot.end_time-1])
+            .then(dbres => {
+                for(i in dbres.rows) {
+                    if(dbres.rows[i].user_pid != req.body.pid) {
+                        isValidReq = false;
+                    }
                 }
-            }
-            if(isValidReq) {
-                db.query("UPDATE parking_times SET price = $5, availability = true WHERE spot_id = $1 AND zone_id = $2 AND time_code BETWEEN $3 AND $4",
-                [req.body.spot.spot_id, req.body.spot.zone_id, req.body.spot.start_time, req.body.spot.end_time-1, req.body.spot.price], (dbres,err) => {
-                    if(err) {
-                        res.status(500).json({message: "Internal Server Error"});
-                    }
-                    else {
-                        res.status(200).json({message: "Success"});
-                    }
-                });
-            }
-            else {
-                res.status(410).json({message: "Don't own all spots trying to sell"});
-            }
-        }).catch(err => {
-            console.log(err);
-        })
-    }
-    else {
-        res.status(400).json({message: "Bad json format"});
+                if(isValidReq) {
+                    db.query("UPDATE parking_times SET price = $5, availability = true WHERE spot_id = $1 AND zone_id = $2 AND time_code BETWEEN $3 AND $4",
+                    [req.body.spot.spot_id, req.body.spot.zone_id, req.body.spot.start_time, req.body.spot.end_time-1, req.body.spot.price], (dbres,err) => {
+                        if(err) {
+                            res.status(500).json({message: "Internal Server Error"});
+                        }
+                        else {
+                            res.status(200).json({message: "Success"});
+                        }
+                    });
+                }
+                else {
+                    res.status(410).json({message: "Don't own all spots trying to sell"});
+                }
+            }).catch(err => {
+                console.log(err);
+            })
+        }
+        else {
+            res.status(400).json({message: "Bad json format"});
+        }
+    } else {
+        res.status(401).json({message: "Bad login"});
     }
 });
 
