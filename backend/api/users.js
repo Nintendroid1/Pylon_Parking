7f6b4e0f9f527573c05636ad83090543cbe39db4
Evan Ott <evan1533@vt.edu>
2020-04-08 11:27:56 -0400

    Modify zones, purchase and users endpoints to cater toward frontend

--- backend/api/users.js
+++ backend/api/users.js
@@ -4,6 +4,7 @@ const { withJWTAuthMiddleware } = require("express-kun");
 const jwt = require("./jwt");
 const db = require("../db");
 const { createSecureRouter, getTokenFromBearer } = require("./auth.js");
+// const { requireLogin } = require("./auth.js");
 
 router.use(express.json());
 
@@ -13,38 +14,56 @@ router.get("/", (req, res) => {
   res.send("This should be login api");
 });
 
-secureRouter.get("/:pid", (req, res) => {
-  let req_token = getTokenFromBearer(req);
-  if (jwt.verifyJWT(req_token, req.params.pid)) {
-    let userInfo = {};
-    db.query("SELECT * FROM users WHERE PID = $1", [req.params.pid])
-      .then(dbres => {
+secureRouter.get("/:pid/spots", function(req, res) {
+  // let req_token = getTokenFromBearer(req);
+  // if (jwt.verifyJWT(req_token, req.params.pid)) {
+  let sellInfo = {};
+  db.query("SELECT * FROM users WHERE PID = $1", [req.params.pid])
+    .then(dbres => {
+      if (dbres.rows[0]) {
+        let tempUser = dbres.rows[0];
+        sellInfo = {
+          pid: tempUser.pid,
+          email: tempUser.email,
+          first_name: tempUser.first_name,
+          last_name: tempUser.last_name,
+          parkingSpotsInfo: []
+        };
+      }
+    })
+    .then(() => {
+      db.query("SELECT Z.zone_name, P.* FROM parking_times P INNER JOIN zones Z ON P.zone_id = Z.zone_id WHERE P.user_pid = $1", [
+        req.params.pid
+      ]).then(dbres => {
         if (dbres.rows[0]) {
-          let tempUser = dbres.rows[0];
-          userInfo = {
-            pid: tempUser.pid,
-            email: tempUser.email,
-            first_name: tempUser.first_name,
-            last_name: tempUser.last_name,
-            parkingSpotsInfo: []
-          };
+          sellInfo.parkingSpotsInfo = dbres.rows;
+        } else {
+          sellInfo.parkingSpotsInfo = [];
         }
-      })
-      .then(() => {
-        db.query("SELECT * FROM parking_times WHERE user_PID = $1", [
-          req.params.pid
-        ]).then(dbres => {
-          if (dbres.rows[0]) {
-            userInfo.parkingSpotsInfo = dbres.rows;
-          } else {
-            userInfo.parkingSpotsInfo = [];
-          }
-          res.status(200).json({ userInfo });
-        });
+        res.status(200).json({ ...sellInfo });
       });
-  } else {
-    res.status(403).json({ message: "Invalid token" });
-  }
+    });
+});
+
+secureRouter.get("/:pid", function(req, res) {
+  // let req_token = getTokenFromBearer(req);
+  // if (jwt.verifyJWT(req_token, req.params.pid)) {
+  let userInfo = {};
+  console.log(req.params.pid);
+  db.query("SELECT * FROM users WHERE PID = $1", [req.params.pid]).then(
+    (dbres, err) => {
+      if (dbres.rows[0]) {
+        let tempUser = dbres.rows[0];
+        userInfo = {
+          pid: tempUser.pid,
+          email: tempUser.email,
+          first_name: tempUser.first_name,
+          last_name: tempUser.last_name
+        };
+      }
+      res.status(200).json({ ...userInfo });
+    }
+  );
 });
 
 router.post("/login", async function(req, res) {
