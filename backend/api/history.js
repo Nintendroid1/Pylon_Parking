e331c0f098f9c8c4a184a6a9855c64e6c4d9edda
nateg98 <nateg98@vt.edu>
2020-04-09 12:11:09 -0400

    no connection to blockchain required

--- backend/api/history.js
+++ backend/api/history.js
@@ -8,20 +8,20 @@ const fetch = require('node-fetch');           // node only; not needed in brows
 const rpc = new JsonRpc('http://localhost:8888', { fetch });
 
 router.get('/', (req, res) => {
-    rpc.get_table_rows({
-        json: true,              // Get the response as json
-        code: 'eosio.token',     // Contract that we target
-        scope: 'alice',         // Account that owns the data
-        table: 'accounts',        // Table name
-        limit: 10,               // Maximum number of rows that we want to get
-        reverse: false,         // Optional: Get reversed data
-        show_payer: false      // Optional: Show ram payer
-    })
-    .then( resp => {
-        const temp = resp.rows;
-        res.status(200).json({temp});
-    });
-    
+    // rpc.get_table_rows({
+    //     json: true,              // Get the response as json
+    //     code: 'eosio.token',     // Contract that we target
+    //     scope: 'alice',         // Account that owns the data
+    //     table: 'accounts',        // Table name
+    //     limit: 10,               // Maximum number of rows that we want to get
+    //     reverse: false,         // Optional: Get reversed data
+    //     show_payer: false      // Optional: Show ram payer
+    // })
+    // .then( resp => {
+    //     const temp = resp.rows;
+    //     res.status(200).json({temp});
+    // });
+    res.status(200).json({"temp":[{"balance":"100.0000 VTP"}]});
 });
 
 module.exports = router;
