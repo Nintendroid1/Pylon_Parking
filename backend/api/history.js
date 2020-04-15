53df0788f4e7f86889fae43e5d48ccc3c13ba473
nateg98 <nateg98@vt.edu>
2020-04-15 16:06:18 -0400

    Put the dummy data in an array

--- backend/api/history.js
+++ backend/api/history.js
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
     //     res.status(500).json({message: "Internal Server Error"});
     // });
     //res.status(200).json({"temp":[{"balance":"100.0000 VTP"}]});
-    res.status(200).json({data: {user: "park.vt", quantity: "10.0000 VTP",spot_id: 123, zone_id: 456, time_code: 1586401381, buyer: "alice", seller: "bob"}});
+    res.status(200).json({data: [{user: "park.vt", quantity: "10.0000 VTP",spot_id: 123, zone_id: 456, time_code: 1586401381, buyer: "alice", seller: "bob"}]});
 });
 
 module.exports = router;
