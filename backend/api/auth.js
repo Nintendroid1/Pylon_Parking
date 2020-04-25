0f77a09102ad48b327125bf7d1bd38578044cd93
Evan Ott <evan1533@vt.edu>
2020-04-24 23:00:13 -0400

    Removed senseless info from auth.js

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -1,31 +1,3 @@
-// const bcrypt = require('bcrypt');
-//
-// const { withJWTAuthMiddleware } = require("express-kun");
-// const secret = "verysecretpasswordthatsdefinatelynotinthesource";
-
-// function getTokenFromBearer(req) {
-//   const authorization = req.headers.authorization;
-//   if (!authorization) {
-//     throw new Error("No Authorization Header");
-//   }
-//   try {
-//     const token = authorization.split("Bearer ")[1];
-//     return token;
-//   } catch {
-//     throw new Error("Invalid Token Format");
-//   }
-// }
-
-// function createSecureRouter(router) {
-//   return withJWTAuthMiddleware(router, secret);
-// }
-
-// module.exports = {
-//   createSecureRouter,
-//   getTokenFromBearer
-// };
-
-
 var passport = require("passport");
 var passportJwt = require("passport-jwt");
 var conf = require("./config.js");
